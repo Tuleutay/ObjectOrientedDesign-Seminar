9d79c7676e2961fa0745a22d11f0bc372b7c5d96
Tuleutay <tuleutay.dairbekov@gmail.com>
2023-03-14 17:51:27 +0600

    2 semenar доделал

--- src/main/java/sem02/Ex2/Ex2_main.java
+++ src/main/java/sem02/Ex2/Ex2_main.java
@@ -1,4 +1,22 @@
 package sem02.Ex2;
 
 public class Ex2_main {
+    public static void main(String[] args) {
+        Human human = new Human();
+        Bicycle bicycle = new Bicycle();
+        human.stop();
+        human.drive(bicycle);
+        human.stop();
+        Roliki roliki=new Roliki();
+        human.drive(roliki);
+        Transport transport = new Bicycle();
+        human.stop();
+        human.drive(transport);
+        Car car = new Car();
+        human.drive(car);
+        human.stop();
+        human.stop();
+        human.drive(car);
+    }
+
 }
--- src/main/java/sem02/Ex2/Roliki.java
+++ src/main/java/sem02/Ex2/Roliki.java
@@ -4,11 +4,11 @@ package sem02.Ex2;
 public class Roliki implements Transport{
     @Override
     public void start() {
-        System.out.println(" старт ролики");
+        System.out.println(" Поехали на роликах ");
     }
 
     @Override
     public void stop() {
-        System.out.println(" стоп ролики");
+        System.out.println(" Снимай ролики");
     }
 }
